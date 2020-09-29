d19df34d892d8fc11ae00556e61cec2ad479be85
RuanL1995 <ruanlehanie@gmail.com>
2020-09-29 10:37:48 +0200

    Test push 2

--- TechGenics/TechGenics/login.cs
+++ TechGenics/TechGenics/login.cs
@@ -16,6 +16,7 @@ namespace TechGenics
         {
             InitializeComponent();
             //test push
+            //test push Ruan 2
         }
 
         loading loadScreen = new loading();
