56b510ea1a3ab1dc4468cac2b829d4d588068468
RuanL1995 <ruanlehanie@gmail.com>
2020-10-07 09:46:17 +0200

    Helper.cs - Gets local server instance and replaces PLACEHOLDEr in app.config with local server info

--- TechGenics/TechGenics/Helper.cs
+++ TechGenics/TechGenics/Helper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using Microsoft.Win32;
 
 namespace TechGenics
 {
@@ -11,7 +12,25 @@ namespace TechGenics
     {
         public static string CnnVal(string name)
         {
-            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+            //Gets local server instance and replaces PLACEHOLDEr in app.config with local server info
+            string svrInstance = "";
+
+            RegistryView registryView = Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Registry32;
+            using (RegistryKey hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, registryView))
+            {
+                RegistryKey instanceKey = hklm.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL", false);
+                if (instanceKey != null)
+                {
+                    foreach (var instanceName in instanceKey.GetValueNames())
+                    {
+                        svrInstance = Environment.MachineName + @"\" + instanceName;
+                    }
+                }
+            }
+
+            string conString = ConfigurationManager.ConnectionStrings[name].ConnectionString;
+            conString = conString.Replace("PLACEHOLDER", svrInstance);
+            return conString;
         }
     }
 }
