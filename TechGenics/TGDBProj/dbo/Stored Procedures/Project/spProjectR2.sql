ca2e0644281238ec5fd5df4c5892148507ca4fc5
RuanL1995 <ruanlehanie@gmail.com>
2020-11-26 16:27:40 +0200

    Push copy control

---
+++ TechGenics/TGDBProj/dbo/Stored Procedures/Project/spProjectR2.sql
@@ -0,0 +1,62 @@
+﻿CREATE PROCEDURE [dbo].[spProjectR2]
+/*
+** Purpose : Gets list of all logged in users projects and tasks based on phase user belongs to
+** Created By :	Ruan
+** Created On :	18 Oct 2020
+*/
+@CurrentUser VARCHAR(50),
+@Initiation Bit = 0,
+@Planning Bit = 0,
+@Execution Bit = 0,
+@CloseOut Bit = 0
+
+AS
+
+BEGIN
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED 
+	SELECT 
+		P.ProjectId,
+		P.ProjectName,
+		P.ProjectPhase,
+		P.ProjectStatus,
+		P.DocumentLocation,
+		T.TaskId,
+		T.TaskName,
+		T.TaskDescription,
+		T.TaskProgress,
+		T.TaskAssignedTo,  
+		T.TaskStatus,
+		U.UserId,
+		U.UserName
+	INTO #FilterByUser
+	FROM 
+		tblTask T
+		INNER JOIN tblUser U ON T.UserId = U.UserId
+		INNER JOIN tblProject P ON T.ProjectId = P.ProjectId
+	WHERE 
+			T.TaskAssignedTo  = @CurrentUser
+
+		
+	SELECT * FROM #FilterByUser
+	WHERE 
+		ProjectPhase LIKE 
+		 CASE 
+			WHEN @Initiation = 1 THEN 
+			'Initiation'			
+			END
+		OR ProjectPhase LIKE 
+		 CASE 			
+			WHEN @Planning = 1 THEN 
+			'Planning'			
+			END
+		OR ProjectPhase LIKE 
+		 CASE 
+			WHEN @Execution = 1 THEN 
+			'Execution'
+			END
+		OR ProjectPhase LIKE 
+		 CASE 
+			WHEN @CloseOut = 1 THEN 
+			'CloseOUt'
+			END				
+END
---
+++ TechGenics/TechGenics/Models/ProjectsAndTasksByUserPhase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechGenics.Models
+{
+    public class ProjectsAndTasksByUserPhase
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectPhase { get; set; }
+        public string ProjectStatus { get; set; }
+        public string DocumentLocation { get; set; }
+        public string TaskId { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public string TaskProgress { get; set; }
+        public string TaskAssigendTo { get; set; }
+        public string TaskStatus { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
